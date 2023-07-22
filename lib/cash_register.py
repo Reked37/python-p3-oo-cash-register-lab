2682a29877d9d40d70775108d6727811a1c8f6b6
Derek Truesdale <dtruesdale37@gmail.com>
2023-07-22 15:43:27 -0500

    somewhat working

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  def __init__(self, discount=0, total=0):
-    # self.item=item
+  def __init__(self, discount=0):
+    self.items=[]
     self.discount=discount
-    self.total=total
+    self.total=0
 
   def add_item(self, item, price, quantity=1):
+    n=quantity
+    while n >0:
+     self.items.append(item)
+     n-=1
     self.total+=price*quantity
 
-  def apply_discount(self, discount=0.8):
-    if discount == 0:
-      print("There is no discount to apply.")
+  def apply_discount(self):
+    if self.discount !=0:
+      self.total-=(self.discount/100.0)*self.total
+      # old_price=self.total
+      # new_price=old_price*(discount/100)
+      # discount_price=(int(new_price))
+      # self.total=discount_price
+      print(f"After the discount, the total comes to ${self.total:g}.")
     else:
-      old_price=self.total
-      new_price=old_price*discount
-      discount_price=(int(new_price))
-      self.total=discount_price
-      print(f"After the discount, the total comes to ${discount_price}.")
+      print("There is no discount to apply.")
+    
+      
