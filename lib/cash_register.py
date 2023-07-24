402fc7dcbfdbffec7a3b7fcb31b0743c2ace58eb
Derek Truesdale <dtruesdale37@gmail.com>
2023-07-24 14:22:08 -0500

    lab completed

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -7,6 +7,7 @@ class CashRegister:
     self.total=0
 
   def add_item(self, item, price, quantity=1):
+    self.price=price
     n=quantity
     while n >0:
      self.items.append(item)
@@ -16,12 +17,28 @@ class CashRegister:
   def apply_discount(self):
     if self.discount !=0:
       self.total-=(self.discount/100.0)*self.total
-      # old_price=self.total
-      # new_price=old_price*(discount/100)
-      # discount_price=(int(new_price))
-      # self.total=discount_price
       print(f"After the discount, the total comes to ${self.total:g}.")
     else:
       print("There is no discount to apply.")
+
+  def void_last_transaction(self):
+    second_last_element=self.items[-2]
+    print(second_last_element)
+    last_element=self.items[-1]
+    if last_element == second_last_element:
+      second_total=self.items.remove(last_element), self.total-self.price
+      subtract= self.total-self.price-self.price
+      self.total=subtract
+    else:
+     new_total=self.items.remove(last_element), self.total-self.price
+     sub=self.total-self.price
+     self.total=sub
+     loop_list=[item for item in self.items]
+     print(loop_list)
+   
+ 
+      
     
+
+
       
